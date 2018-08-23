fc5a8e678ff6d6deb77c82f7efbede926a06c95a
drpsy77 <dpsy@free.fr>
2018-08-23 16:34:59 +0200

    Version OK

--- Variable.h
+++ Variable.h
@@ -1,10 +1,10 @@
 typedef struct Player Player;
 struct Player
 {
-  char          name[8];
-  unsigned int  hand1;
-  unsigned int  hand2;
-  unsigned int  money;
-  unsigned int  bet;
-  unsigned char play;
+  char            name[8];
+  unsigned int    hand1;
+  unsigned int    hand2;
+  unsigned int    money;
+  unsigned int    bet;
+  unsigned char   play;
 };
--- main.c
+++ main.c
@@ -19,9 +19,10 @@ void APlot(int,int,char,int);
 #define FLIP 5
 #define NOCARD 0
 
-unsigned int i,j,k, crd, curs, kee, inibet;
+unsigned int i=0,j=0,k=0, crd=0, curs=0, kee=0;
+unsigned int inibet=0;
 
-unsigned int rd1, rd2, nb1, nb2, crd1, crd2;
+unsigned int rd1=0, rd2=0, nb1=0, nb2=0, crd1=0, crd2=0;
 unsigned int cards[52], table[5];
 
 unsigned char nbPlayers, turn, selec, allPlay, cont;
@@ -35,11 +36,15 @@ Player p [4] = {{"P1", 0, 0, 600, 0, 1},
 char *strJoue   = "JOUE       ";
 char *strNoJoue = "NE JOUE PAS";
 
+char message[38];
 
 void prepare()
 {
 	for(i=0;i<52;i++)
 		cards[i] = i;
+	
+	for(i=0;i<5;i++)
+		table[i] = 0;
 }
 
 
@@ -66,8 +71,9 @@ void shuffle()
 }
 
 void pause(int ps){
-	for(i=0;i<ps;i++)
-		for(j=0;j<256;j++)
+	int mi,ni;
+	for(mi=0;mi<ps;mi++)
+		for(ni=0;ni<256;ni++)
 			;
 }
 
@@ -171,12 +177,13 @@ void main()
 						AdvancedPrint(20,2+i,strNoJoue);
 					}
 				}
+				/*
 				for(i=nbPlayers;i<4;i++){
 					p[i].play = 0;
 					APlot(2,2+i,0,1);
 					APlot(1,2+i,23,1);
 					APlot(20,2+i,' ',11);
-				}
+				} */
 
 				kee = get();
 				if(kee>=49 && kee <=52) {
@@ -218,13 +225,13 @@ void main()
 			printCard(2+k*6,9, table[k]%13+1, table[k]/13+1);
 			pause(4);
 		}
-		
-		for(i=0;i<nbPlayers;i++)
-			AdvancedPrint(21,2+i,itoa(p[i].bet));
-		
 				
 		//Pi Play
 		for(i=0;i<nbPlayers;i++){
+		
+			for(k=0;k<nbPlayers;k++)
+				AdvancedPrint(21,2+k,itoa(p[k].bet));
+		
 			for(j=16;j<22;j++) {
 				APlot(2,j,5,1);
 				APlot(3,j,109,34);
@@ -241,13 +248,15 @@ void main()
 				APlot(8,23,' ',18);
 				APlot(8,25,' ',22);		
 				APlot(1,2,22,1);
+				printCard( 2, 16, NOCARD, VIDE);
 				printCard( 2+6, 16, NOCARD, VIDE);
-				printCard( 2+2*6, 16, NOCARD, VIDE);
 			
 				printCard(2,16, NOCARD, FLIP);
 			
 				pause(4);
-				printCard(2,16, p[i].hand1 %13+1, p[i].hand1 /13+1);
+				printCard(2,16, p[i].hand1%13+1, p[i].hand1/13+1);
+				//sprintf(message,"cartes du joueur:%d %d",p[i].hand1, p[i].hand2);
+				AdvancedPrint(2,0,message);
 				pause(4);
 				printCard(8,16, NOCARD, FLIP);
 				pause(4);
@@ -258,7 +267,7 @@ void main()
 				{
 					AdvancedPrint(5,23,"+ 1"); AdvancedPrint(10,23,"+ 5"); AdvancedPrint(15,23,"+10"); AdvancedPrint(20,23,"+50");
 					AdvancedPrint(5,24,"- 1"); AdvancedPrint(10,24,"- 5"); AdvancedPrint(15,24,"-10"); AdvancedPrint(20,24,"-50");
-					AdvancedPrint(10,25,"VALIDER"); AdvancedPrint(30,24,"      "); AdvancedPrint(30,24,itoa(p[0].bet));
+					AdvancedPrint(10,25,"VALIDER"); APlot(30,24,' ',6); AdvancedPrint(30,24,itoa(p[i].bet));
 					AdvancedPrint(9,27,"  [FLECHES]  [ENTER]  ");
 					if(curs == 1){APlot( 4,23,22,1);APlot( 8,23,23,1);}else APlot( 4,23,23,1);
 					if(curs == 2){APlot( 9,23,22,1);APlot(13,23,23,1);}else APlot( 9,23,23,1);
@@ -313,30 +322,22 @@ void main()
 					}
 					if(kee==13)				// ENTER
 					{
-						if(curs == 1){if(p[i].bet<p[i].money   )p[i].bet++;}
-						if(curs == 2){if(p[i].bet<p[i].money-5 )p[i].bet+=5;}
-						if(curs == 3){if(p[i].bet<p[i].money-10)p[i].bet+=10;}
-						if(curs == 4){if(p[i].bet<p[i].money-50)p[i].bet+=50;}
-						if(curs == 5){if(p[i].bet>inibet )      p[i].bet--;}
-						if(curs == 6){if(p[i].bet>(inibet+4))   p[i].bet-=5;}
-						if(curs == 7){if(p[i].bet>(inibet+9))   p[i].bet-=10;}
-						if(curs == 8){if(p[i].bet>(inibet+49))  p[i].bet-=50;}
-						if(curs == 9){selec = 0;}
+						if(curs == 1){ if(p[i].bet < p[i].money   ) p[i].bet++;}
+						if(curs == 2){ if(p[i].bet < p[i].money-5 ) p[i].bet+=5;}
+						if(curs == 3){ if(p[i].bet < p[i].money-10) p[i].bet+=10;}
+						if(curs == 4){ if(p[i].bet < p[i].money-50) p[i].bet+=50;}
+						if(curs == 5){ if(p[i].bet > inibet       ) p[i].bet--;}
+						if(curs == 6){ if(p[i].bet > (inibet+4)   ) p[i].bet-=5;}
+						if(curs == 7){ if(p[i].bet > (inibet+9)   ) p[i].bet-=10;}
+						if(curs == 8){ if(p[i].bet > (inibet+49)  ) p[i].bet-=50;}
+						if(curs == 9){ selec = 0;}
 					}
 				}
 			
 				for(k=23;k<28;k++)
 					APlot(3,k,' ',36);
 			}
-			
-			for(i=0;i<nbPlayers;i++)
-				AdvancedPrint(21,2+i,itoa(p[i].bet));
-		}	
-			
-		for(k=23;k<28;k++)
-			APlot(3,k,' ',36);
-		
-		
+		}
 		get();
 	}
 }
